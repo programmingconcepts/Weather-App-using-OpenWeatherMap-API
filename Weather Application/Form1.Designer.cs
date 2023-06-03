0dd63ff24e022986c7fe28ee2ead2dad75c6f692
Syed Abbas <79309742+programmingconcepts@users.noreply.github.com>
2023-06-03 20:42:14 +0500

    Add files via upload

---
+++ Weather Application/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Weather_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBCity = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labWindSpeed = new System.Windows.Forms.Label();
+            this.labSunrise = new System.Windows.Forms.Label();
+            this.labSunset = new System.Windows.Forms.Label();
+            this.labHumidity = new System.Windows.Forms.Label();
+            this.labTemp = new System.Windows.Forms.Label();
+            this.labDetails = new System.Windows.Forms.Label();
+            this.labCondition = new System.Windows.Forms.Label();
+            this.picWeatherIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeatherIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TBCity
+            // 
+            this.TBCity.Location = new System.Drawing.Point(47, 25);
+            this.TBCity.Name = "TBCity";
+            this.TBCity.Size = new System.Drawing.Size(168, 20);
+            this.TBCity.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(248, 25);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Condition:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Details:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Temperature";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Humidity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Sunset:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Sunrise:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Wind Speed:";
+            // 
+            // labWindSpeed
+            // 
+            this.labWindSpeed.AutoSize = true;
+            this.labWindSpeed.Location = new System.Drawing.Point(158, 317);
+            this.labWindSpeed.Name = "labWindSpeed";
+            this.labWindSpeed.Size = new System.Drawing.Size(69, 13);
+            this.labWindSpeed.TabIndex = 15;
+            this.labWindSpeed.Text = "Wind Speed:";
+            // 
+            // labSunrise
+            // 
+            this.labSunrise.AutoSize = true;
+            this.labSunrise.Location = new System.Drawing.Point(158, 262);
+            this.labSunrise.Name = "labSunrise";
+            this.labSunrise.Size = new System.Drawing.Size(45, 13);
+            this.labSunrise.TabIndex = 14;
+            this.labSunrise.Text = "Sunrise:";
+            // 
+            // labSunset
+            // 
+            this.labSunset.AutoSize = true;
+            this.labSunset.Location = new System.Drawing.Point(158, 287);
+            this.labSunset.Name = "labSunset";
+            this.labSunset.Size = new System.Drawing.Size(43, 13);
+            this.labSunset.TabIndex = 13;
+            this.labSunset.Text = "Sunset:";
+            // 
+            // labHumidity
+            // 
+            this.labHumidity.AutoSize = true;
+            this.labHumidity.Location = new System.Drawing.Point(158, 231);
+            this.labHumidity.Name = "labHumidity";
+            this.labHumidity.Size = new System.Drawing.Size(50, 13);
+            this.labHumidity.TabIndex = 12;
+            this.labHumidity.Text = "Humidity:";
+            // 
+            // labTemp
+            // 
+            this.labTemp.AutoSize = true;
+            this.labTemp.Location = new System.Drawing.Point(158, 199);
+            this.labTemp.Name = "labTemp";
+            this.labTemp.Size = new System.Drawing.Size(67, 13);
+            this.labTemp.TabIndex = 11;
+            this.labTemp.Text = "Temperature";
+            // 
+            // labDetails
+            // 
+            this.labDetails.AutoSize = true;
+            this.labDetails.Location = new System.Drawing.Point(158, 166);
+            this.labDetails.Name = "labDetails";
+            this.labDetails.Size = new System.Drawing.Size(42, 13);
+            this.labDetails.TabIndex = 10;
+            this.labDetails.Text = "Details:";
+            // 
+            // labCondition
+            // 
+            this.labCondition.AutoSize = true;
+            this.labCondition.Location = new System.Drawing.Point(158, 138);
+            this.labCondition.Name = "labCondition";
+            this.labCondition.Size = new System.Drawing.Size(54, 13);
+            this.labCondition.TabIndex = 9;
+            this.labCondition.Text = "Condition:";
+            // 
+            // picWeatherIcon
+            // 
+            this.picWeatherIcon.Location = new System.Drawing.Point(376, 60);
+            this.picWeatherIcon.Name = "picWeatherIcon";
+            this.picWeatherIcon.Size = new System.Drawing.Size(60, 45);
+            this.picWeatherIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picWeatherIcon.TabIndex = 16;
+            this.picWeatherIcon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picWeatherIcon);
+            this.Controls.Add(this.labWindSpeed);
+            this.Controls.Add(this.labSunrise);
+            this.Controls.Add(this.labSunset);
+            this.Controls.Add(this.labHumidity);
+            this.Controls.Add(this.labTemp);
+            this.Controls.Add(this.labDetails);
+            this.Controls.Add(this.labCondition);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.TBCity);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picWeatherIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBCity;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labWindSpeed;
+        private System.Windows.Forms.Label labSunrise;
+        private System.Windows.Forms.Label labSunset;
+        private System.Windows.Forms.Label labHumidity;
+        private System.Windows.Forms.Label labTemp;
+        private System.Windows.Forms.Label labDetails;
+        private System.Windows.Forms.Label labCondition;
+        private System.Windows.Forms.PictureBox picWeatherIcon;
+    }
+}
+
---
+++ Weather Application/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Weather_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string API = "e6dda21fc3786c71e5b0d2cbce52bedc";
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+
+        public void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = String.Format(@"http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", TBCity.Text, API);
+                var json = web.DownloadString(url);
+                var results = JsonConvert.DeserializeObject<Weather_Info.root>(json);
+
+                Weather_Info.root root = results;
+
+                labCondition.Text = root.weather[0].main;
+
+                labDetails.Text = root.weather[0].description;
+
+                labTemp.Text = root.main.temp.ToString() + " K";
+                labHumidity.Text = root.main.humidity.ToString();
+                labSunrise.Text = getDateTime(root.sys.sunrise).ToShortTimeString();
+                labSunset.Text = getDateTime(root.sys.sunset).ToShortTimeString();
+                labWindSpeed.Text = root.wind.speed.ToString() + "m/s";
+
+                picWeatherIcon.ImageLocation = "http://openweathermap.org/img/w/" + root.weather[0].icon + ".png";
+            }
+        }
+
+        public DateTime getDateTime(double millisec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+            day = day.AddMilliseconds(millisec).ToLocalTime();
+
+            return day;
+        }
+    }
+}
